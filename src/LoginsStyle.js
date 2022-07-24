cadb976bf5221cfef1757d12814bf6a8eaffc1ed
JoEunsoo <dcpg95@gmail.com>
2022-07-24 22:23:44 +0900

    update

--- src/LoginsStyle.js
+++ src/LoginsStyle.js
@@ -1,2 +1,78 @@
+import React from "react";
 import styled from "styled-components";
 
+
+const Container = styled.div`
+    display:flex;
+    justify-content: center;
+    margin: 50px;
+    h1{
+        margin-top : 40px;
+    }
+    .acc_modal {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 650px;
+        height: 650px;
+        border: solid 1px black;
+    }
+    .join_btn{
+        width: 450px;
+        height: 40px;
+        background: black;
+        color : #ffffff;
+        margin: 50px;
+    }
+    .move_login{
+        cursor: pointer;
+    }
+    
+  
+`
+
+const AccountInput = styled.div`
+position: relative;
+
+    input{
+            width: 450px;
+            height: 40px;
+            margin-top: 50px;
+            outline: none;
+            border:none;
+            border-bottom: 1px solid #999;
+        }
+        .input_msg{
+            position:absolute;
+            left: 10px;
+            top : 95px;
+            color: red;
+            font-size: 13px;
+        }
+        label {
+            position: absolute;
+            left : 0px;
+            top : 35px;
+            color : #999;
+            transition : all 0.5s ease;
+        }
+        
+        input:focus + label {
+            top : 40px;
+            font-size : 13px;
+            color: blue;
+        }
+        input:focus {
+            border-bottom: 1px solid blue;
+        }
+       
+        
+        /* input:valid + label::before {
+            content: '✓';
+            color: green;
+} */
+   
+`
+
+
+export {AccountInput , Container, }
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import userData from "./data";
 import { useNavigate,useLocation } from "react-router-dom";
+import { AccountInput, Container } from "./LoginsStyle";
+import axios from "axios";
 
 
 const SignUP = ({setUserData}) => {
@@ -9,21 +11,25 @@ const SignUP = ({setUserData}) => {
         id : '', 
         password : '',
     });
+    const[inputMsg , setInputMsg] = useState('아이디를 입력해주세요');
 
     const newUser = (key) => (e) => { //회원가입 페이지 아이디, 비번 입력 값 받아오기
         setNewUserInfo({...newUserInfo, [key] : e.target.value });
     }
 
     // 기존 userData 의 id 값과 중복여부 체크 , 빈칸 입력시 경고, 제대로 입력했을 경우 회원데이터에 추가
-    const createNewUser = (key) => (e) => {
+    const createNewUser = () => {
         
-        const checkUser = userData.filter( (user) => user.id === newUserInfo.id);
-        console.log(checkUser)
-        setNewUserInfo('');
-        if(!newUserInfo.id && !newUserInfo.password) return alert('아이디 비밀번호를 입력하세요!');
-        if(checkUser.length) return alert('중복된 아이디 입니다!');
-        setUserData([...userData,newUserInfo]);
-        
+        // const checkUser = userData.filter( (user) => user.id === newUserInfo.id);
+        // console.log(checkUser)
+        // setNewUserInfo('');
+        // if(!newUserInfo.id && !newUserInfo.password) return alert('아이디 비밀번호를 입력하세요!');
+        // if(checkUser.length) return alert('중복된 아이디 입니다!');
+        // setUserData([...userData,newUserInfo]);
+        // setNewUserInfo({id: '' , password : ''})
+        console.log(newUserInfo)
+        axios.post('http://localhost:4848/login',{...newUserInfo})
+        .then(res => console.log(res))
         
     }
 
@@ -36,37 +42,28 @@ const SignUP = ({setUserData}) => {
     };
     
     return(
-        <container>
-            <div>
-                <input 
-                type="text" 
-                placeholder="아이디"
-                onChange={newUser('id')}/>
-                <input type="reset" />
-            </div>
-            <div>
-                <input 
-                type="password" 
-                placeholder="비밀번호"
-                onChange={newUser('password')}/>
-            </div>
-            
-            {/* 인풋 초기화 하는거 테스트중 */}
-            <div>
-            <input
-            type="text"
-            value={inputText}
-            placeholder="입력하세요"
-            onChange={onChangeInput}
-            />
-            <button onClick={onReset}>Reset</button>
-            </div>
-            {/* 인풋 초기화 하는거 테스트중 */}
-            
-            <button onClick={createNewUser}>가입ㄱㄱ</button>
-            <button onClick={ () => navigate('/login')}>로그인페이지로</button>
-            <button onClick={() => console.log(userData)}>클릭해보샘</button>
-        </container>
+        <> 
+            <Container>
+                <div className="acc_modal">
+                    <h1>회원가입</h1>
+                    <AccountInput>
+                        <input type="text" placeholder="아이디입력" onChange={newUser('id')} />
+                        <label for='id'>아이디</label>
+                        <span className="input_msg">{inputMsg}</span>
+                    </AccountInput>
+                    <AccountInput>
+                        <input  type="password" onChange={newUser('password')}/>
+                        <label for="pw">비밀번호</label>
+                    </AccountInput>
+                    <AccountInput>
+                        <input type="text" onChange={newUser('userName')}/>
+                        <label for="username">닉네임</label>
+                    </AccountInput>
+                    <button className="join_btn" onClick={createNewUser}>가입하기</button>
+                    <div className="move_login" onClick={ () => navigate('/login')}>로그인하기</div>
+                </div>
+            </Container>
+        </>
     )
 }
 
